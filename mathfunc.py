872a4cebec3929abbaa2a7e833f6c8539c325f73
mofawzy <110044321+mofawzy31@users.noreply.github.com>
2022-11-29 09:39:37 +0200

    Update mathfunc.py

--- mathfunc.py
+++ mathfunc.py
@@ -21,3 +21,10 @@ def fib(x):
         f2 = f3
         f3 = f1 + f2
     return v 
+#matrix transpose
+def M_trans(a):
+    h=[[0 for i in range(len(a))] for i in range(len(a[0]))]
+    for i in range(len(a[0])):
+        for z in range(len(a)):
+            h[i][z]=a[z][i]
+    return h
